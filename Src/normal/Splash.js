6b9d5180e0064e718e6edd21dd3ec7ff5238547f
unknown <beramoumit1122@gmail.com>
2024-05-28 10:46:33 +0530

    splash screen time change

--- Src/normal/Splash.js
+++ Src/normal/Splash.js
@@ -9,7 +9,7 @@ const Splash = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       navigation.navigate('SignUp');
-    }, 2000);
+    }, 3000);
     return () => clearTimeout(timer);
   }, [navigation]);
 
